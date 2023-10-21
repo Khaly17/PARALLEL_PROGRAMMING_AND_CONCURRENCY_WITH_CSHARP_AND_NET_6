55fd6ffe0a6203e5152acffb10e04d990c9b3af2
Khaly DIENG <SNDIENGK@modelsis.net>
2023-10-21 23:30:31 +0000

    – Canceling a thread in the console application

---
+++ chapter_1/BackgroundPingConsoleApp_cancel/NetworkingWork.cs
@@ -0,0 +1,15 @@
+namespace BackgroundPingConsoleApp_cancel
+{
+    internal class NetworkingWork
+    {
+        public void CheckNetworkStatus(object data)
+        {
+            var cancelToken = (CancellationToken)data;
+            while (!cancelToken.IsCancellationRequested)
+            {
+                bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+                Console.WriteLine($"Is network available? Answer: {isNetworkUp}");
+            }
+        }
+    }
+}
